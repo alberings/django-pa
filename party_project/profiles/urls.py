81676be36dbdbc0bb6baf4b8b0fc73dab6aa1c34
alberings <71380595+alberings@users.noreply.github.com>
2024-02-03 22:41:29 +0200

    Delete party_project directory

--- party_project/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register, profile, custom_logout
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('profile/', profile, name='profile'),
-    path('logout/', custom_logout, name='logout'),
-]
